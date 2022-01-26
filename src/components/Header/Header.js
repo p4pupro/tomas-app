6a9cf4ce713942e9fe2f40be1893b94c17d66d7d
p4pupro <domingoperezguerrero@gmail.com>
2022-01-26 08:40:45 +0000

    fixed main path to gh-pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,16 +11,16 @@ export const Header = () => {
                     <Link to="tomas-app/" className="link-text">Tomas</Link>
                 </div>
                 <div className="header-button">
-                    <Link to="cacas" className="link-text">Cacas</Link>
+                    <Link to="tomas-app/cacas" className="link-text">Cacas</Link>
                 </div>
                 <div className="header-button">
-                    <Link to="pis" className="link-text">Pis</Link>
+                    <Link to="tomas-app/pis" className="link-text">Pis</Link>
                 </div>
                 <div className="header-button">
-                    <Link to="peso" className="link-text">Peso</Link>
+                    <Link to="tomas-app/peso" className="link-text">Peso</Link>
                 </div>
                 <div className="header-button">
-                    <Link to="altura" className="link-text">Altura</Link>
+                    <Link to="tomas-app/altura" className="link-text">Altura</Link>
                 </div>
             </div>
         </div>
