eaeb1e71d96fd430b9da151ccafb1584655d2434
p4pupro <domingoperezguerrero@gmail.com>
2022-02-09 20:15:34 +0000

    radio buttons bigger full width

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export const Layout = () => { 
     return (
         <div className="row">
-            <div className="col-12 col-s-12" style={{ width: '100%' }}>
+            <div className="col-12 col-s-12">
                 <div className="aside">
                     <Outlet />
                 </div>
